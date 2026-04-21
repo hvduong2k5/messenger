26496272ecac0086337e0fda244547b951c85231
hvduong <hd2005555@gmail.com>
2026-04-21 20:16:46 +0700

    Initial commit

---
+++ backend-api/messenger/src/test/java/com/team12345/messenger/MessengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team12345.messenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
