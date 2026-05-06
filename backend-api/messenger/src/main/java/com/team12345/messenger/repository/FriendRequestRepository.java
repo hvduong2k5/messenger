743a45c0aea06357d6efea1d5cd77d2b57e38c60
Danh <meowmeow01aa@gmail.com>
2026-05-06 23:35:40 +0700

    feat/31-repository-friendrepository

--- backend-api/messenger/src/main/java/com/team12345/messenger/repository/FriendRequestRepository.java
+++ backend-api/messenger/src/main/java/com/team12345/messenger/repository/FriendRequestRepository.java
@@ -3,13 +3,17 @@ package com.team12345.messenger.repository;
 import com.team12345.messenger.entity.FriendRequest;
 import com.team12345.messenger.entity.FriendRequestId;
 import com.team12345.messenger.entity.FriendRequestStatus;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
+ 
 @Repository
 public interface FriendRequestRepository extends JpaRepository<FriendRequest, FriendRequestId> {
-    List<FriendRequest> findByReceiverIdAndStatus(Long receiverId, FriendRequestStatus status);
-    List<FriendRequest> findBySenderIdAndStatus(Long senderId, FriendRequestStatus status);
+    @EntityGraph(value = "FriendRequest.withReceiver", type = EntityGraph.EntityGraphType.FETCH)
+    Page<FriendRequest> findBySender_IdAndStatus(Long senderId, FriendRequestStatus status, Pageable pageable);
+    @EntityGraph(value = "FriendRequest.withSender", type = EntityGraph.EntityGraphType.FETCH)
+    Page<FriendRequest> findByReceiver_IdAndStatus(Long receiverId, FriendRequestStatus status, Pageable pageable);
 }
