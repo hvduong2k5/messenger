4e94519452c46581d2aafd516d5e5f07cda111de
hvduong <hd2005555@gmail.com>
2026-05-07 09:18:33 +0700

    feat(#39): add MessageService interface and UserResponseDTO class

--- backend-api/messenger/src/main/java/com/team12345/messenger/dto/response/UserResponseDTO.java
+++ backend-api/messenger/src/main/java/com/team12345/messenger/dto/response/UserResponseDTO.java
@@ -1,14 +1,15 @@
 package com.team12345.messenger.dto.response;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserResponseDTO {
-
     private Long id;
     private String username;
     private String email;
