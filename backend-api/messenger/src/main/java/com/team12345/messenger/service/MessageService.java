28dd482380660de9a3abd46ed09a9578252808b6
hvduong <hd2005555@gmail.com>
2026-05-10 21:02:54 +0700

    refactor(#39): update MessageService to use MessageResponseDTO

--- backend-api/messenger/src/main/java/com/team12345/messenger/service/MessageService.java
+++ backend-api/messenger/src/main/java/com/team12345/messenger/service/MessageService.java
@@ -1,13 +1,13 @@
 package com.team12345.messenger.service;
 
 import com.team12345.messenger.dto.request.MessageRequestDTO;
-import com.team12345.messenger.dto.response.DetailMessageResponseDTO;
+import com.team12345.messenger.dto.response.MessageResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MessageService {
 
-    DetailMessageResponseDTO saveMessage(MessageRequestDTO requestDTO);
+    MessageResponseDTO saveMessage(MessageRequestDTO requestDTO);
 
-    Page<DetailMessageResponseDTO> getMessagesByConversation(Long conversationId, Pageable pageable);
+    Page<MessageResponseDTO> getMessagesByConversation(Long conversationId, Pageable pageable);
 }
