4a5af62fd669583305a7c7563cc634ae9eaae584
hvduong <hd2005555@gmail.com>
2026-05-11 15:54:49 +0700

    feat(#45): add UpdateProfileRequestDTO for user profile updates

---
+++ backend-api/messenger/src/main/java/com/team12345/messenger/dto/request/UpdateProfileRequestDTO.java
@@ -0,0 +1,17 @@
+package com.team12345.messenger.dto.request;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateProfileRequestDTO {
+    
+    @Size(max = 50)
+    private String status;
+}
