b6f3b941fec7785f93a951a92c1c9b2f90eda385
hvduong <hd2005555@gmail.com>
2026-05-11 21:17:56 +0700

    feat(#42): add method to check if friend relationship exists

--- backend-api/messenger/src/main/java/com/team12345/messenger/repository/UserFriendRepository.java
+++ backend-api/messenger/src/main/java/com/team12345/messenger/repository/UserFriendRepository.java
@@ -1,15 +1,23 @@
 package com.team12345.messenger.repository;
 
+import com.team12345.messenger.entity.UserFriend;
+import com.team12345.messenger.entity.UserFriendId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.team12345.messenger.entity.UserFriend;
-import com.team12345.messenger.entity.UserFriendId;
+
+import java.util.List;
 
 @Repository
 public interface UserFriendRepository extends JpaRepository<UserFriend, UserFriendId> {
-    @EntityGraph(value = "UserFriend.withFriendInfo", type = EntityGraph.EntityGraphType.FETCH)
+
+    @EntityGraph(value = "UserFriend.withFriendInfo")
+    List<UserFriend> findById_UserId(Long userId);
+
+    @EntityGraph(value = "UserFriend.withFriendInfo")
     Page<UserFriend> findById_UserId(Long userId, Pageable pageable);
+
+    boolean existsById_UserIdAndId_FriendId(Long userId, Long friendId);
 }
