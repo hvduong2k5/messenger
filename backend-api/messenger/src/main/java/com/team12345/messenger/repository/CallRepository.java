a614a52ef00d912b87c03be71503ec06a6c4dd4b
Pham Tuan <phamtuanrr77@gmail.com>
2026-05-12 12:00:55 +0700

    feat(#43): add method to retrieve calls by caller or receiver ID

--- backend-api/messenger/src/main/java/com/team12345/messenger/repository/CallRepository.java
+++ backend-api/messenger/src/main/java/com/team12345/messenger/repository/CallRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.team12345.messenger.entity.Call;
 
+import java.util.List;
+
 @Repository
 public interface CallRepository extends JpaRepository<Call, Long>{
     @EntityGraph(value = "Call.withReceiver", type = EntityGraph.EntityGraphType.FETCH)
@@ -14,4 +16,6 @@ public interface CallRepository extends JpaRepository<Call, Long>{
 
     @EntityGraph(value = "Call.withParticipants", type = EntityGraph.EntityGraphType.FETCH)
     Slice<Call> findByCaller_IdOrReceiver_Id(Long callerId, Long receiverId, Pageable pageable);
+
+    List<Call> findByCaller_IdOrReceiver_Id(Long callerId, Long receiverId);
 }
