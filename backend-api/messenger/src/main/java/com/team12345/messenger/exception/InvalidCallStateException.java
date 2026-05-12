22c2be7630c8fd933ed90a56fed1303a47654996
Pham Tuan <phamtuanrr77@gmail.com>
2026-05-12 12:01:56 +0700

    feat(#43): add InvalidCallStateException for handling invalid call states

---
+++ backend-api/messenger/src/main/java/com/team12345/messenger/exception/InvalidCallStateException.java
@@ -0,0 +1,7 @@
+package com.team12345.messenger.exception;
+
+public class InvalidCallStateException extends RuntimeException {
+    public InvalidCallStateException(String message) {
+        super(message);
+    }
+}
