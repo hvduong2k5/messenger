6592d7ae2d748ce14c42e339c198cd23bea46d37
hvduong <hd2005555@gmail.com>
2026-05-12 21:41:58 +0700

    feat(#135): enable JPA auditing for automatic timestamp management

--- backend-api/messenger/src/main/java/com/team12345/messenger/MessengerApplication.java
+++ backend-api/messenger/src/main/java/com/team12345/messenger/MessengerApplication.java
@@ -2,7 +2,9 @@ package com.team12345.messenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MessengerApplication {
 
