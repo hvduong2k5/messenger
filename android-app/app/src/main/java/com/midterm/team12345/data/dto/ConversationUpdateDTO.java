01432984dd1d1164576bd88c3ec5aa2e87907333
hvduong <hd2005555@gmail.com>
2026-05-14 00:25:18 +0700

    feat(#99): add ConversationUpdateDTO for updating conversation details

---
+++ android-app/app/src/main/java/com/midterm/team12345/data/dto/ConversationUpdateDTO.java
@@ -0,0 +1,27 @@
+package com.midterm.team12345.data.dto;
+
+public class ConversationUpdateDTO {
+    private String name;
+    private String avatarUrl;
+
+    public ConversationUpdateDTO(String name, String avatarUrl) {
+        this.name = name;
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+}
