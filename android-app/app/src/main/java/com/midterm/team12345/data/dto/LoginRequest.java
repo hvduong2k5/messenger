97be0daac069a44cd0aab99bfa01f9696b3d48bf
hvduong <hd2005555@gmail.com>
2026-05-13 22:08:10 +0700

    refactor(#79): update login request to use usernameOrEmail field

--- android-app/app/src/main/java/com/midterm/team12345/data/dto/LoginRequest.java
+++ android-app/app/src/main/java/com/midterm/team12345/data/dto/LoginRequest.java
@@ -1,16 +1,21 @@
 package com.midterm.team12345.data.dto;
 
+import com.google.gson.annotations.SerializedName;
+
 public class LoginRequest {
-    private String username;
+    @SerializedName("usernameOrEmail")
+    private String usernameOrEmail;
+    
+    @SerializedName("password")
     private String password;
 
-    public LoginRequest(String username, String password) {
-        this.username = username;
+    public LoginRequest(String usernameOrEmail, String password) {
+        this.usernameOrEmail = usernameOrEmail;
         this.password = password;
     }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getUsernameOrEmail() { return usernameOrEmail; }
+    public void setUsernameOrEmail(String usernameOrEmail) { this.usernameOrEmail = usernameOrEmail; }
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
