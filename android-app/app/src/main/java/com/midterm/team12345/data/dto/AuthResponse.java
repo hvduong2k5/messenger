d7ab7df457489fe24568f0810f9fc82be729cd60
hvduong <hd2005555@gmail.com>
2026-05-13 10:57:29 +0700

    feat(#160): add AuthResponse class for authentication data handling

---
+++ android-app/app/src/main/java/com/midterm/team12345/data/dto/AuthResponse.java
@@ -0,0 +1,18 @@
+package com.midterm.team12345.data.dto;
+
+public class AuthResponse {
+    private String accessToken;
+    private String tokenType;
+    private UserDTO user;
+
+    public AuthResponse() {}
+
+    public String getAccessToken() { return accessToken; }
+    public void setAccessToken(String accessToken) { this.accessToken = accessToken; }
+
+    public String getTokenType() { return tokenType; }
+    public void setTokenType(String tokenType) { this.tokenType = tokenType; }
+
+    public UserDTO getUser() { return user; }
+    public void setUser(UserDTO user) { this.user = user; }
+}
