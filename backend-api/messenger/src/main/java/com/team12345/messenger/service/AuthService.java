2d3431d1d348ea3d5a5282fa8edbd688d9c76acb
Pham Tuan <phamtuanrr77@gmail.com>
2026-05-13 22:10:20 +0700

    feat(#44): add forgot and reset password methods to AuthService

--- backend-api/messenger/src/main/java/com/team12345/messenger/service/AuthService.java
+++ backend-api/messenger/src/main/java/com/team12345/messenger/service/AuthService.java
@@ -1,7 +1,9 @@
 package com.team12345.messenger.service;
 
+import com.team12345.messenger.dto.request.ForgotPasswordRequestDTO;
 import com.team12345.messenger.dto.request.LoginRequestDTO;
 import com.team12345.messenger.dto.request.RegisterRequestDTO;
+import com.team12345.messenger.dto.request.ResetPasswordRequestDTO;
 import com.team12345.messenger.dto.response.AuthResponseDTO;
 
 public interface AuthService {
@@ -9,4 +11,10 @@ public interface AuthService {
     AuthResponseDTO register(RegisterRequestDTO registerRequestDTO);
 
     AuthResponseDTO login(LoginRequestDTO loginRequestDTO);
+
+    void forgotPassword(ForgotPasswordRequestDTO forgotPasswordRequestDTO);
+
+    void resetPassword(ResetPasswordRequestDTO resetPasswordRequestDTO);
+
+    void logout(Long userId);
 }
