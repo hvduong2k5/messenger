825dd2e9831c37114ab1b61b9129657f6a7da684
hvduong <hd2005555@gmail.com>
2026-05-14 00:51:02 +0700

    feat(#96): add methods for creating conversation and retrieving friends

--- android-app/app/src/main/java/com/midterm/team12345/data/repository/ChatRepository.java
+++ android-app/app/src/main/java/com/midterm/team12345/data/repository/ChatRepository.java
@@ -2,9 +2,13 @@ package com.midterm.team12345.data.repository;
 
 import androidx.lifecycle.LiveData;
 import com.midterm.team12345.data.dto.ConversationResponse;
+import com.midterm.team12345.data.dto.ConversationRequestDTO;
+import com.midterm.team12345.data.dto.UserDTO;
 import com.midterm.team12345.util.Resource;
 import java.util.List;
 
 public interface ChatRepository {
     LiveData<Resource<List<ConversationResponse>>> getConversations();
+    LiveData<Resource<List<UserDTO>>> getFriends();
+    LiveData<Resource<ConversationResponse>> createConversation(ConversationRequestDTO request);
 }
