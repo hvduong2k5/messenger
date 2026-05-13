ef7f5ed2c4b9a10af824a078cf724709c709e89e
Pham Tuan <phamtuanrr77@gmail.com>
2026-05-13 22:12:20 +0700

    feat(#44): add ResetPasswordRequestDTO for password reset requests

---
+++ backend-api/messenger/src/main/java/com/team12345/messenger/dto/request/ResetPasswordRequestDTO.java
@@ -0,0 +1,16 @@
+package com.team12345.messenger.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class ResetPasswordRequestDTO {
+
+    @NotBlank(message = "Token is required")
+    private String token;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String newPassword;
+}
