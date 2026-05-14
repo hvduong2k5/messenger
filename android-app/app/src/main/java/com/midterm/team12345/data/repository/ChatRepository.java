def2333ff9d16bcb35acecfbd92cee02cbf1c8db
hvduong <hd2005555@gmail.com>
2026-05-14 14:02:38 +0700

    feat(#177): add method to fetch user profile in ChatRepository

--- android-app/app/src/main/java/com/midterm/team12345/data/repository/ChatRepository.java
+++ android-app/app/src/main/java/com/midterm/team12345/data/repository/ChatRepository.java
@@ -7,6 +7,7 @@ import com.midterm.team12345.data.dto.MessageRequestDTO;
 import com.midterm.team12345.data.dto.MessageResponse;
 import com.midterm.team12345.data.dto.MqttMessageDTO;
 import com.midterm.team12345.data.dto.UserDTO;
+import com.midterm.team12345.data.dto.UserProfileResponseDTO;
 import com.midterm.team12345.util.Resource;
 import java.util.List;
 
@@ -16,6 +17,7 @@ public interface ChatRepository {
     LiveData<Resource<ConversationResponse>> createConversation(ConversationRequestDTO request);
     LiveData<Resource<List<MessageResponse>>> getMessages(Long conversationId);
     LiveData<Resource<MessageResponse>> sendMessage(MessageRequestDTO request);
+    LiveData<Resource<UserProfileResponseDTO>> getMyProfile();
     LiveData<MqttMessageDTO> getRealTimeMessages();
     LiveData<Boolean> getConnectionStatus();
 }
