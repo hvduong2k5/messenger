c59707ee5bde6a910d6e625fd0552ff6c6b21b29
hvduong <hd2005555@gmail.com>
2026-05-14 08:49:38 +0700

    feat(#125): add method to retrieve real-time messages from MQTT

--- android-app/app/src/main/java/com/midterm/team12345/data/repository/ChatRepository.java
+++ android-app/app/src/main/java/com/midterm/team12345/data/repository/ChatRepository.java
@@ -3,6 +3,7 @@ package com.midterm.team12345.data.repository;
 import androidx.lifecycle.LiveData;
 import com.midterm.team12345.data.dto.ConversationResponse;
 import com.midterm.team12345.data.dto.ConversationRequestDTO;
+import com.midterm.team12345.data.dto.MqttMessageDTO;
 import com.midterm.team12345.data.dto.UserDTO;
 import com.midterm.team12345.util.Resource;
 import java.util.List;
@@ -11,4 +12,5 @@ public interface ChatRepository {
     LiveData<Resource<List<ConversationResponse>>> getConversations();
     LiveData<Resource<List<UserDTO>>> getFriends();
     LiveData<Resource<ConversationResponse>> createConversation(ConversationRequestDTO request);
+    LiveData<MqttMessageDTO> getRealTimeMessages();
 }
