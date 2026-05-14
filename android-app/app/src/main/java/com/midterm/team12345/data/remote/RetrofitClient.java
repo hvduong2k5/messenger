0e2640217a96e62c606521e8c55f80e14185bfc6
hvduong <hd2005555@gmail.com>
2026-05-14 14:02:59 +0700

    refactor(#177): update BASE_URL in RetrofitClient for API integration

--- android-app/app/src/main/java/com/midterm/team12345/data/remote/RetrofitClient.java
+++ android-app/app/src/main/java/com/midterm/team12345/data/remote/RetrofitClient.java
@@ -16,7 +16,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL = "http://192.168.1.166:8080";
+    private static final String BASE_URL = "http://10.38.249.57:8080";
     private static Retrofit retrofit = null;
 
     public static synchronized Retrofit getClient(Context context) {
