cd4e39ea423d8d84baa6119a4d503317ea9a8e4d
hvduong <hd2005555@gmail.com>
2026-05-14 13:11:41 +0700

    feat(#177): add message retrieval and sending methods in ChatRepository

--- android-app/app/src/main/java/com/midterm/team12345/data/repository/ChatRepository.java
+++ android-app/app/src/main/java/com/midterm/team12345/data/repository/ChatRepository.java
@@ -3,6 +3,8 @@ package com.midterm.team12345.data.repository;
 import androidx.lifecycle.LiveData;
 import com.midterm.team12345.data.dto.ConversationResponse;
 import com.midterm.team12345.data.dto.ConversationRequestDTO;
+import com.midterm.team12345.data.dto.MessageRequestDTO;
+import com.midterm.team12345.data.dto.MessageResponse;
 import com.midterm.team12345.data.dto.MqttMessageDTO;
 import com.midterm.team12345.data.dto.UserDTO;
 import com.midterm.team12345.util.Resource;
@@ -12,6 +14,8 @@ public interface ChatRepository {
     LiveData<Resource<List<ConversationResponse>>> getConversations();
     LiveData<Resource<List<UserDTO>>> getFriends();
     LiveData<Resource<ConversationResponse>> createConversation(ConversationRequestDTO request);
+    LiveData<Resource<List<MessageResponse>>> getMessages(Long conversationId);
+    LiveData<Resource<MessageResponse>> sendMessage(MessageRequestDTO request);
     LiveData<MqttMessageDTO> getRealTimeMessages();
     LiveData<Boolean> getConnectionStatus();
 }
