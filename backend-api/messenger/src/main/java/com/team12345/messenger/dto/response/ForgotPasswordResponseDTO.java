e6e080f73de1e752c6e7d301b7899747f17fa078
Pham Tuan <phamtuanrr77@gmail.com>
2026-05-15 09:07:33 +0700

    feat(#44): add ForgotPasswordResponseDTO for password reset responses

---
+++ backend-api/messenger/src/main/java/com/team12345/messenger/dto/response/ForgotPasswordResponseDTO.java
@@ -0,0 +1,16 @@
+package com.team12345.messenger.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ForgotPasswordResponseDTO {
+    private String message;
+    private boolean success;
+}
+
