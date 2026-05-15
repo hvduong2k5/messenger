7fba2fa839c64e98b433a79198403384ad8af074
hvduong <hd2005555@gmail.com>
2026-05-16 00:55:48 +0700

    refactor(#121): add AttachmentResponseDTO for handling attachment data

---
+++ android-app/app/src/main/java/com/midterm/team12345/data/dto/response/AttachmentResponseDTO.java
@@ -0,0 +1,62 @@
+package com.midterm.team12345.data.dto.response;
+
+import java.time.LocalDateTime;
+
+public class AttachmentResponseDTO {
+    private Long id;
+    private String url;
+    private String type;
+    private Integer fileSize;
+    private String publicId;
+    private LocalDateTime uploadedAt;
+
+    public AttachmentResponseDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(Integer fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public String getPublicId() {
+        return publicId;
+    }
+
+    public void setPublicId(String publicId) {
+        this.publicId = publicId;
+    }
+
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(LocalDateTime uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+}
