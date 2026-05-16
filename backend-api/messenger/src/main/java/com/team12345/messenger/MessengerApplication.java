d29419c649fcbacb6c47b56725ebb209fdc06b97
hvduong <hd2005555@gmail.com>
2026-05-16 16:22:02 +0700

    refactor(#186): move JpaAuditing configuration to separate class

--- backend-api/messenger/src/main/java/com/team12345/messenger/MessengerApplication.java
+++ backend-api/messenger/src/main/java/com/team12345/messenger/MessengerApplication.java
@@ -2,9 +2,7 @@ package com.team12345.messenger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class MessengerApplication {
 
