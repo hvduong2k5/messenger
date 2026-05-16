6a1d5b449992c868fc9476513f961f82032d51d9
Pham Tuan <phamtuanrr77@gmail.com>
2026-05-16 10:58:06 +0700

    refactor(#44): convert EmailService to an interface

--- backend-api/messenger/src/main/java/com/team12345/messenger/service/EmailService.java
+++ backend-api/messenger/src/main/java/com/team12345/messenger/service/EmailService.java
@@ -1,21 +1,7 @@
 package com.team12345.messenger.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+public interface EmailService {
 
-@Service
-@RequiredArgsConstructor
-public class EmailService {
+    public void sendPasswordResetEmail(String to, String otp);
 
-    private final JavaMailSender mailSender;
-
-    public void sendPasswordResetEmail(String to, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("Password Reset OTP");
-        message.setText("OTP to reset your password: " + otp);
-        mailSender.send(message);
-    }
 }
