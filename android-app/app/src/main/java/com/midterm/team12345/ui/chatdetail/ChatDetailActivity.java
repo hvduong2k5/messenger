f1b7821f31019daa417690d95a4ab711bc9b4bac
hvduong <hd2005555@gmail.com>
2026-05-16 17:39:28 +0700

    refactor(#185): remove unused ChatRepositoryImpl import in ChatDetailActivity

--- android-app/app/src/main/java/com/midterm/team12345/ui/chatdetail/ChatDetailActivity.java
+++ android-app/app/src/main/java/com/midterm/team12345/ui/chatdetail/ChatDetailActivity.java
@@ -5,7 +5,7 @@ import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
-import com.midterm.team12345.data.repository.ChatRepositoryImpl;
+
 import com.midterm.team12345.databinding.ActivityChatDetailBinding;
 import com.midterm.team12345.utils.Resource;
 
