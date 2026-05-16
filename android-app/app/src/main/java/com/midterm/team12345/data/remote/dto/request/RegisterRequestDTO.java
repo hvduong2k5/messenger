59dc8197674b51624c6c1390cc2b2c27b0de0bf3
hvduong <hd2005555@gmail.com>
2026-05-16 14:51:28 +0700

    feat(#185): add validation annotations to RegisterRequestDTO fields

--- android-app/app/src/main/java/com/midterm/team12345/data/remote/dto/request/RegisterRequestDTO.java
+++ android-app/app/src/main/java/com/midterm/team12345/data/remote/dto/request/RegisterRequestDTO.java
@@ -2,14 +2,27 @@ package com.midterm.team12345.data.remote.dto.request;
 
 import com.google.gson.annotations.SerializedName;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class RegisterRequestDTO {
     @SerializedName("username")
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 100, message = "Username must be between 3 and 100 characters")
+    @Pattern(regexp = "^\\S+$", message = "Username must not contain spaces")
     private String username;
 
     @SerializedName("email")
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @Size(max = 150, message = "Email must not exceed 150 characters")
     private String email;
 
     @SerializedName("password")
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
     private String password;
 
     public RegisterRequestDTO(String username, String email, String password) {
