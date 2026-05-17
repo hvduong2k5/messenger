47999493ae8aec84e4c13585037edeb30b1d8d8e
hvduong <hd2005555@gmail.com>
2026-05-17 23:39:03 +0700

    chore(#202): downgrade compile and target SDK versions to 35

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.midterm.team12345"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.midterm.team12345"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
