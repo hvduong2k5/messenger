94bb23c49070d5123646f351ef4aefee6c902ff9
hvduong <hd2005555@gmail.com>
2026-05-17 18:10:01 +0700

    build(#194): add annotation library dependency

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.annotation)
 
     // Retrofit & OkHttp
     implementation(libs.retrofit)
