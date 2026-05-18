330207f5d4956b387a5f2226a82a54aacab9d32b
hvduong <hd2005555@gmail.com>
2026-05-18 10:06:16 +0700

    refactor(#190): update FriendRepository methods for friend management

--- android-app/app/src/main/java/com/midterm/team12345/domain/repository/FriendRepository.java
+++ android-app/app/src/main/java/com/midterm/team12345/domain/repository/FriendRepository.java
@@ -7,10 +7,12 @@ import com.midterm.team12345.utils.Resource;
 import java.util.List;
 
 public interface FriendRepository {
-    LiveData<Resource<FriendRequestResponseDTO>> sendFriendRequest(Long receiverId);
-    LiveData<Resource<Void>> acceptFriendRequest(Long senderId);
-    LiveData<Resource<Void>> declineFriendRequest(Long senderId);
+    LiveData<Resource<Void>> sendFriendRequest(Long receiverId);
+    LiveData<Resource<Void>> acceptFriendRequest(Long requestId);
+    LiveData<Resource<Void>> rejectFriendRequest(Long requestId);
+    LiveData<Resource<Void>> removeFriend(Long friendId);
     LiveData<Resource<Void>> unfriend(Long friendId);
+    LiveData<Resource<List<UserResponseDTO>>> getFriends();
     LiveData<Resource<List<UserResponseDTO>>> getFriendsList();
-    LiveData<Resource<List<FriendRequestResponseDTO>>> getPendingFriendRequests();
+    LiveData<Resource<List<FriendRequestResponseDTO>>> getPendingRequests();
 }
