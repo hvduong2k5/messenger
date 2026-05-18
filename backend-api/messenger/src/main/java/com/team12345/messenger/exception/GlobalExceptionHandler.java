3a06258f911db79691327095395ff0ebef63d2ff
hvduong <hd2005555@gmail.com>
2026-05-18 10:51:29 +0700

    feat(#206): add handler for UserNotFoundException in GlobalExceptionHandler

--- backend-api/messenger/src/main/java/com/team12345/messenger/exception/GlobalExceptionHandler.java
+++ backend-api/messenger/src/main/java/com/team12345/messenger/exception/GlobalExceptionHandler.java
@@ -13,6 +13,11 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
