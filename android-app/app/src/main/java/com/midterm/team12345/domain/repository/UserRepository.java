09a64f3d10f4af3e1c79ebef6eb77aca7589d0f3
Nguyen-Trang-25 <nguyentrang.050925@gmail.com>
2026-05-24 10:29:09 +0700

    feat(#100): implement user profile screen and edit profile integration

--- android-app/app/src/main/java/com/midterm/team12345/domain/repository/UserRepository.java
+++ android-app/app/src/main/java/com/midterm/team12345/domain/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.midterm.team12345.domain.repository;
 
 import androidx.lifecycle.LiveData;
+import com.midterm.team12345.data.local.entity.UserEntity;
 import com.midterm.team12345.data.remote.dto.request.UpdateProfileRequestDTO;
 import com.midterm.team12345.data.remote.dto.response.PageResponse;
 import com.midterm.team12345.data.remote.dto.response.UserProfileResponseDTO;
@@ -10,6 +11,7 @@ import okhttp3.MultipartBody;
 
 public interface UserRepository {
     LiveData<Resource<UserProfileResponseDTO>> getMyProfile();
+    LiveData<UserEntity> getLocalUser(Long userId);
     LiveData<Resource<UserProfileResponseDTO>> getUserProfile(Long id);
     LiveData<Resource<UserProfileResponseDTO>> updateProfile(UpdateProfileRequestDTO request);
     LiveData<Resource<UserProfileResponseDTO>> updateAvatar(MultipartBody.Part file);
