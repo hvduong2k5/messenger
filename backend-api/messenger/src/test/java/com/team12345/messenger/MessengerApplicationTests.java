c7d07b61bc2226a56cde6e67156157b2a12a101b
Danh <meowmeow01aa@gmail.com>
2026-05-29 00:16:47 +0700

    test(#254): remove hardcoded test property source from MessengerApplicationTests

--- backend-api/messenger/src/test/java/com/team12345/messenger/MessengerApplicationTests.java
+++ backend-api/messenger/src/test/java/com/team12345/messenger/MessengerApplicationTests.java
@@ -5,12 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb",
-    "spring.datasource.driverClassName=org.h2.Driver",
-    "spring.datasource.username=sa",
-    "spring.datasource.password=password"
-})
 class MessengerApplicationTests {
 
     @Test
