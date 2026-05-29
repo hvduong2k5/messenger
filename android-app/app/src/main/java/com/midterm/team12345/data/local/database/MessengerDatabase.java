f087d6bcbb5d38bc29b3cdb32c29d304d26d3b29
hvduong <hd2005555@gmail.com>
2026-05-29 11:21:10 +0700

    fix(#256): add unique message_id index and set messageId in entity

--- android-app/app/src/main/java/com/midterm/team12345/data/local/database/MessengerDatabase.java
+++ android-app/app/src/main/java/com/midterm/team12345/data/local/database/MessengerDatabase.java
@@ -31,7 +31,7 @@ import com.midterm.team12345.data.local.entity.UserEntity;
         SyncQueueEntity.class,
         DraftMessageEntity.class
     },
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 @TypeConverters({Converters.class})
